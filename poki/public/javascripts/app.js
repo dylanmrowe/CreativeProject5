f712c232edbe2a86598bd70c215f23f3e7e06afa
TsaiChihHan <tsaizhihan@gmail.com>
2016-03-05 17:45:59 -0700

    Begin

---
+++ poki/public/javascripts/app.js
@@ -0,0 +1,68 @@
+var app = window.angular.module('app', [])
+
+app.factory('pokemonFetcher', pokemonFetcher)
+app.controller('mainCtrl', mainCtrl)
+
+
+function pokemonFetcher ($http) {
+
+  var API_ROOT = 'pokemon'
+  return {
+    get: function () {
+      return $http
+        .get(API_ROOT)
+        .then(function (resp) {
+          return resp.data
+        })
+    },
+    tryit: function() {
+      var politics = '/politics';
+
+      return $http
+        .get(politics)
+        .then(function (resp) {
+
+          console.log("Get Worked");
+          console.log(resp.data);
+          return resp.data
+        })
+    }
+  }
+
+}
+
+function mainCtrl ($scope, pokemonFetcher) {
+
+  $scope.pokemon = []
+
+  pokemonFetcher.get()
+    .then(function (data) {
+      $scope.pokemon = data
+    })
+
+  pokemonFetcher.tryit()
+    .then(function (data) {
+      console.log("tryit");
+      console.log(data);
+    })
+
+    $("#button").click(
+        function(e){
+        var myQuery = document.getElementById("inputbox").value;
+        var urls = "http://pokeapi.co/api/v2/pokemon/"+myQuery;
+        console.log(urls);
+        console.log("Test");
+        $.ajax({
+          url: urls,
+          dataType: 'json',
+          success:  function(data){
+            console.log(data.name);
+            console.log(data.height);
+            console.log(data.sprites.front_default);
+            console.log(data.forms);
+            console.log(data.forms);
+            $("#result").append("<p>"+data.name+"</p>"+"<img src="+data.sprites.front_default+">"+"<img src=sprites.pokecheck.org/i/006.gif>");
+          }
+       });
+      });
+  }
